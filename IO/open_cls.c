8b48a0b056185d71b84b4a9d2c3bde670b2b91d6
yueyue <1419894578@qq.com>
2020-05-23 05:49:59 -0700

    first commit

---
+++ IO/open_cls.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int ac, char **av)
+{
+	int exit_status = EXIT_SUCCESS;
+	FILE *input;
+	while(*++av != NULL)
+	{
+		input = fopen(*av, "r");
+		if(input == NULL)
+		{
+			perror(*av);
+			exit_status = EXIT_FAILURE;
+			continue;
+		}
+
+		input = NULL;
+
+		if(fclose(input) != 0)
+		{
+			perror("fclose");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return exit_status;
+}
